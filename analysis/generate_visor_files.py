4c959c9eece5e218ad737b4ed0842fb5eedd8143
nriddiford <nickriddiford@me.com>
2020-10-21 13:59:25 +0200

    Update

--- analysis/generate_visor_files.py
+++ analysis/generate_visor_files.py
@@ -18,71 +18,81 @@ def set_regions():
         '2R': {'start': 5e6, 'stop': 15e6, 'buffer': 1e6},
         '3L': {'start': 5e6, 'stop': 15e6, 'buffer': 1e6},
         '3R': {'start': 5e6, 'stop': 15e6, 'buffer': 1e6},
-        '4': {'start': 4e5, 'stop': 9e5, 'buffer': 2e4},
+        '4': {'start': 2e5, 'stop': 9e5, 'buffer': 2e4},
         'X': {'start': 5e6, 'stop': 15e6, 'buffer': 1e6},
         'Y': {'start': 1e6, 'stop': 2e6, 'buffer': 1e5}
     }
 
-    svs_to_generate = {
-        1: [50, 'deletion', 'None', 0],
+    tumour_svs = {
+        1: [200, 'deletion', 'None', 0],
         2: [100, 'deletion', 'None', 0],
         3: [20, 'tandem duplication', 1, 0],
         4: [5, 'inversion', 'None', 0],
-        5: [10, 'deletion', 'None', 0],
-        6: [50, 'deletion', 'None', 0],
+        5: [300, 'deletion', 'None', 0],
+        6: [2, 'deletion', 'None', 0],
         7: [100, 'tandem duplication', 1, 0],
-        8: [20, 'deletion', 'None', 0],
-        9: [5, 'deletion', 'None', 0],
+        8: [50, 'deletion', 'None', 0],
+        9: [25, 'deletion', 'None', 0],
         10: [1, 'inversion', 'None', 0]
+    }
+
+    normal_svs = {
+        3: '2L',
+        6: '3R',
     }
 
     chroms = ['2L', '2R', '3L', '3R', '4', 'X', 'Y']
 
-    return chroms, sv_regions, svs_to_generate
+    return chroms, sv_regions, tumour_svs, normal_svs
 
 def parse_bed(options):
 
-    svs_per_chrom = 5
-    chroms, sv_regions, svs_to_generate = set_regions()
+    chroms, sv_regions, tumour_svs, normal_svs = set_regions()
+
+    th1, th2 = 'tumour_h1.bed', 'tumour_h2.bed'
+    nh1, nh2 = 'normal_h1.bed', 'normal_h2.bed'
 
 
-    with open(options.bedfile, 'w') as bed_out:
+    with open(th1, 'w') as t1, open(th2, 'w') as t2, open(nh1, 'w') as n1, open(nh2, 'w') as n2:
         for chrom in chroms:
-            start, end = int(sv_regions[chrom]['start']), int(sv_regions[chrom]['stop'])
-            walker = start
-            length = end - start
-            for sv in svs_to_generate.keys():
-                sv_length = svs_to_generate[sv][0] * 1000
-                items = '\t'.join(map(str, svs_to_generate[sv][1:]))
-                l = '\t'.join(map(str, [chrom, walker, walker + sv_length, items]))
-                # print(l)
-                bed_out.write(l + '\n')
-                walker += sv_length + int(sv_regions[chrom]['buffer'])
+            region_start, window_end = int(sv_regions[chrom]['start']), int(sv_regions[chrom]['stop'])
+            sv_start = region_start
+            for sv in tumour_svs.keys():
+                if sv > options.svs_per_chrom: continue
+                sv_length = tumour_svs[sv][0] * 1000
+                items = '\t'.join(map(str, tumour_svs[sv][1:]))
+                sv_end = sv_start + sv_length
+                if sv_end >= window_end:
+                    print("The end of this SV %s is bigger than the region on chrom %s!" % (tumour_svs[sv], chrom))
+                l = '\t'.join(map(str, [chrom, sv_start, sv_end, items]))
 
+                write_haplotypes(l, sv, t1, t2)
+                if sv in normal_svs and normal_svs[sv] == chrom:
+                    write_haplotypes(l, sv, n1, n2)
 
+                sv_start += sv_length + int(sv_regions[chrom]['buffer'])
 
     return True
+
+
+def write_haplotypes(l, sv, h1, h2):
+    if sv % 2 != 0:
+        h1.write(l + '\n')
+    else:
+        h2.write(l + '\n')
 
 
 def get_args():
     parser = OptionParser()
 
-    parser.add_option("-b", "--bedfile", dest="bedfile", action="store", help=".bed file to write SVs to")
-    # parser.add_option("-g", "--genome", dest="genome", action="store", help="Genome fasta file")
-    # parser.add_option("-w", "--window", dest="window", action="store", type="int", help="Window to slide over sequence. [Default 5]")
-    # parser.add_option("-l", "--locus", dest="locus", action="store", help="chr:start-end")
-    # parser.add_option("--event", dest="event", action="store", help="Inspect one event")
+    parser.add_option("-d", "--tumour-depth", dest="depth", action="store", help="Average sequencing depth for tumour sample. [10]")
+    parser.add_option("-n", "--svs-per-chrom", dest="svs_per_chrom", action="store", help="Number of SVs per chromosomes. [10]")
 
-    parser.set_defaults(bedfile='svs.bed')
+    parser.set_defaults(bedfile='svs.bed', depth=30, svs_per_chrom=10)
 
     options, args = parser.parse_args()
 
-    if not options.bedfile:
-        parser.print_help()
-        print()
-        sys.exit("[!] Must provide a bed file. Exiting.")
-    else:
-        return options, args
+    return options, args
 
 
 def main():
