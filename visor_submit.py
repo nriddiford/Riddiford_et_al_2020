738a9d1ff09e30cd5c8fd296c3b205531bbfc1e2
nriddiford <nickriddiford@me.com>
2020-10-23 12:30:51 +0200

    Add wrapper for visor running

---
+++ visor_submit.py
@@ -0,0 +1,21 @@
+import os, sys
+from optparse import OptionParser
+
+
+def submit_jobs():
+    purities = [100, 80, 60, 40, 20]
+
+    tumour_id = 1
+
+    for p in purities:
+        tid = tumour_id
+        nid = tumour_id + 1
+        tumour_sample = "visorR%s" % tid
+        normal_sample = "visorR%s" % nid
+
+        print("qsub -v VAR1=%s,VAR2=%s,VAR3=%s -o log/%s.runlog -j oe -N %s.visor" % (tumour_sample, normal_sample, p, tumour_sample, tumour_sample))
+
+        tumour_id += 2
+
+
+submit_jobs()
