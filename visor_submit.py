85c886e4b91f759bc1328e07621a9a6d9e6bdf0d
nriddiford <nickriddiford@gmail.com>
2020-10-26 10:24:39 +0100

    Update

--- visor_submit.py
+++ visor_submit.py
@@ -13,7 +13,7 @@ def submit_jobs():
         tumour_sample = "visorR%s" % tid
         normal_sample = "visorR%s" % nid
 
-        cmd = "qsub -v VAR1=%s,VAR2=%s,VAR3=%s -o log/%s.runlog -j oe -N %s.visor run_visor.pbs" % (tumour_sample, normal_sample, p, tumour_sample, tumour_sample)
+        cmd = "qsub -v VAR1=%s,VAR2=%s,VAR3=%s -o visor/log/%s.runlog -j oe -N %s.visor run_visor.pbs" % (tumour_sample, normal_sample, p, tumour_sample, tumour_sample)
 
         print(cmd)
         os.system(cmd)
