a1e50d8d1717675b79c7207f2ff9de262f24ded5
nriddiford <nickriddiford@me.com>
2020-10-26 13:37:00 +0100

    Run per sample

--- visor_submit.py
+++ visor_submit.py
@@ -6,6 +6,8 @@ def submit_jobs():
     purities = [100, 80, 60, 40, 20]
 
     tumour_id = 1
+    tumour_coverage=30
+    normal_coverage=50
 
     for p in purities:
         tid = tumour_id
@@ -13,10 +15,13 @@ def submit_jobs():
         tumour_sample = "visorR%s" % tid
         normal_sample = "visorR%s" % nid
 
-        md = "qsub -v VAR1=%s,VAR2=%s,VAR3=%s -o visor/log/%s.runlog -j oe -N %s.visor run_visor.pbs" % (tumour_sample, normal_sample, p, tumour_sample, tumour_sample)
+        t_cmd = "qsub -v VAR1=%s,VAR2=%s,VAR3=%s,VAR4=%s -o visor/log/%s.runlog -j oe -N %s.visor run_visor.pbs" % (tumour_sample, p, 'tumour', tumour_coverage, tumour_sample, tumour_sample)
+        n_cmd = "qsub -v VAR1=%s,VAR2=%s,VAR3=%s,VAR4=%s -o visor/log/%s.runlog -j oe -N %s.visor run_visor.pbs" % (normal_sample, p, 'normal', normal_coverage, normal_sample, normal_sample)
 
-        print(cmd)
-        os.system(cmd)
+        print(t_cmd)
+        print(n_cmd)
+        # os.system(t_cmd)
+        # os.system(n_cmd)
 
         tumour_id += 2
 
