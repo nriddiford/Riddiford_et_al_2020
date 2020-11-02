fb02c0f46df9ab996e444817ef4ef68936b7da21
nriddiford <nickriddiford@me.com>
2020-11-02 15:45:38 +0100

    Update

--- analysis/annotate_simSVs.py
+++ analysis/annotate_simSVs.py
@@ -4,31 +4,87 @@ import os, re, sys
 import json
 from collections import defaultdict
 
+test = False
 
 truth_set_in = 'data/visor_svs.bed'
 vars_in = 'data/all_sim_samples_merged_filt.txt'
+if test:
+    vars_in = 'data/R11_merged_filt_annotated.txt'
+
+out_dir = 'data/'
+
+
+#####################################
+# Things that don't work:          ##
+#   - No tandem dups are created   ##
+#   - del on 3R clearly missed     ##
+#   - no vars on Y...              ##
+#####################################
+fix_errors = True
 
 
 def get_vars(simvars_in):
-    bed = []
+    bed = defaultdict(dict)
+    svs = []
+
     with open(simvars_in, 'r') as vars:
         for l in vars:
             parts = l.rstrip().split('\t')
-            bed.append([parts[4], parts[5], parts[7], parts[3], parts])
+            orig_sv_type = parts[3]
+            if parts[0] != 'sample':
+                orig_sv_type = parts[-2].split(';')[0].split('=')[1]
+            sample, c, s, e, t = parts[0], parts[4], parts[5], parts[7], convert_types(orig_sv_type)
 
-    return bed
+            # if test and c != '3R': continue
+
+            svs.append([c, s, e, t, parts])
+            bed.setdefault(sample, []).append([c, s, e, t])
+
+    write_bed(bed)
+
+    return svs
+
+
+def convert_types(sv):
+    sv_types = {
+        'BND': 'inversion',
+        'INV': 'inversion',
+        'DEL': 'deletion',
+        'DUP': 'duplication',
+        'TANDUP': 'tandem duplication'
+    }
+
+    if sv in sv_types:
+        return sv_types[sv]
+
+    return sv.lower()
 
 
 def get_truth(truth_file):
 
     d = defaultdict(lambda: defaultdict(dict))
 
+    unmappable = {
+        '2L_10625000': 1
+    }
+
     count = 0
     with open(truth_file, 'r') as truth:
         for l in truth:
-            count += 1
             parts = l.rstrip().split('\t')
-            d[parts[0]][parts[1]] = parts
+            c, s, t = parts[0], parts[1], parts[3]
+
+            # if test and c != '3R': continue
+
+            k = '_'.join([c, s])
+            if fix_errors:
+                if c == 'Y': continue
+                if t == 'tandem duplication': continue
+                if k in unmappable: continue
+
+
+            d[c][s] = parts
+            count += 1
 
     return d, count
 
@@ -39,7 +95,11 @@ def annotate_vars(vars, truth):
 
     annotated_vars = []
     true_positive_count = defaultdict(lambda: defaultdict(int))
+    true_positive_types = defaultdict(lambda: defaultdict(int))
+    false_positive_types = defaultdict(lambda: defaultdict(int))
+
     seen_events = defaultdict(lambda: defaultdict(int))
+    keys = defaultdict(lambda: defaultdict(int))
 
     for v in vars:
         c, s, e, t, l = v
@@ -50,35 +110,97 @@ def annotate_vars(vars, truth):
             continue
 
         true_positive_count[sample]
-        window = range(int(s) - 10000, int(s) + 10000, 1)
+        window = range(int(s) - 10, int(s) + 10, 1)
 
-        for pos in window:
+        if caller == 'Control-Freec':
+            window = range(int(s) - 10000, int(s) + 10000, 1)
+
+        for pos in iter(window):
             pos = str(pos)
-            true_positive = False
-            if true_calls[c] and true_calls[c][pos]:
-                k = '_'.join([c, pos])
-                true_positive_count[sample][k] += 1
-                true_positive = True
+            k = '_'.join([c, pos])
+            true_positive, true_positive_count = is_true(true_calls, true_positive_count, c, pos, sample, t)
+            if true_positive and not seen_events[sample][event]: break
+        keys['truth']
 
-            # if true_positive:
-            #     print("True positive: [%s] %s:%s-%s", (sample, c, pos, e))
+        if true_positive and not seen_events[sample][event]:
+            keys[sample][k] = [event, 'tp', t]
+            true_positive_types[sample][t] += 1
 
+        elif not true_positive and not seen_events[sample][event]:
+            keys[sample][k] = [event, 'fp', t]
+            false_positive_types[sample][t] += 1
+
+        seen_events[sample][event] += 1
         annotated_vars.append('\t'.join(l))
 
-    # print(json.dumps(true_positive_count, indent=2))
+    keys = add_false_negatives(keys, true_calls)
+
+    write_error_rates(keys)
 
     total_count = 0
     for s in true_positive_count:
         count = len(true_positive_count[s])
         total_count += count
         perc = (int(count)/true_call_count) * 100
-        print("Sample: %s : %s / %s true positives found [%s]%%" % (s, count, true_call_count, perc))
+        print("Sample: %s : %s / %s true positives found [%s]%%" % (s, count, true_call_count, int(perc)))
 
     no_samples = len(true_positive_count)
-    print("Total: %s / %s [%s]%%" % (total_count, true_call_count*no_samples, total_count/(true_call_count*no_samples)*100))
+    print("Total: %s / %s [%s]%%" % (total_count, true_call_count*no_samples, round(total_count/(true_call_count*no_samples)*100)))
 
     with open('data/all_sim_samples_merged_filt_annotated.txt', 'w') as f:
         f.write('\n'.join(map(str, annotated_vars)))
+
+
+def add_false_negatives(keys, true_calls):
+    for s in keys:
+        for c in true_calls:
+            for pos in true_calls[c]:
+                k = '_'.join([c, pos])
+                keys['truth'][k] = ['-', 'tp', true_calls[c][pos][3]]
+                if k not in keys[s]:
+                    print("false negative: [%s] %s:%s-%s" % (true_calls[c][pos][3], true_calls[c][pos][0], true_calls[c][pos][1], true_calls[c][pos][2]))
+                    keys[s][k] = ['-', 'fn', true_calls[c][pos][3]]
+
+
+    return keys
+
+
+def is_true(true_calls, true_positive_count, c, s, sample, sv_type):
+    true_positive = False
+    if c in true_calls and s in true_calls[c]:
+        k = '_'.join([c, s])
+        true_positive_count[sample][k] += 1
+        true_positive = True
+    return true_positive, true_positive_count
+
+
+def write_bed(bed):
+    for sample in bed.keys():
+        if sample == 'sample': continue
+        bed_out = os.path.join(out_dir, sample + '_svs.bed')
+        with open(bed_out, 'w') as b:
+            for c, s, e, t in bed[sample]:
+                l = '\t'.join(map(str, [c, s, e, t]))
+                b.write(l + '\n')
+    return
+
+
+def write_error_rates(vars):
+    print(json.dumps(vars, indent=4, sort_keys=True))
+
+    df_out = os.path.join(out_dir, 'error_rates.txt')
+    with open(df_out, 'w') as out:
+        l = '\t'.join(['sample', 'event', 'chrom', 'start', 'type', 'error_group'])
+        out.write(l + '\n')
+        for s in vars.keys():
+            for key in vars[s]:
+                l = vars[s][key]
+                e, et, sv = l
+                c, pos = key.split('_')
+                l = '\t'.join(map(str, [s, e, c, pos, sv, et]))
+                # print(l)
+                out.write(l + '\n')
+                # print("sample:%s, tp total: %s, tp types: %s, tp counts: %s, fp types: %s, fp counts: %s" % (s, len(tp_counts[s]), tp[s].keys(), tp[s].values(), fp[s].keys(), fp[s].values()))
 
 
 annotate_vars(vars_in, truth_set_in)
